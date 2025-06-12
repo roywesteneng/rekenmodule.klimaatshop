f11062550587a36d23e435dfb09fe33cf5951179
roywesteneng <roy@klimaatshop.nl>
2025-06-12 16:09:36 +0200

    main.js
    nog een update

--- main.js
+++ main.js
@@ -136,6 +136,6 @@ function berekenAfstand() {
   const afstand = Math.pow(10, ((db + correctie - norm) / 20));
   document.getElementById('resultaat').innerHTML =
   '👉 Met dit airco-model heb je minstens <strong>' + afstand.toFixed(1) +
-  ' meter</strong> meter afstand tot de erfgrens nodig om geen problemen met regelgeving of geluidsoverlast te krijgen. Dit is nodig om onder de geluidsnorm van <strong>' + norm + ' dB(A)</strong> te blijven.';
+  ' meter</strong> afstand tot de erfgrens nodig om geen problemen met regelgeving of geluidsoverlast te krijgen. Dit is nodig om onder de wettelijke geluidsnorm van <strong>' + norm + ' dB(A)</strong> te blijven.';
 }
 
